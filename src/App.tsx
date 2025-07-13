d129c1c70373d501e5111f6dac9c7b2034525d50
AvFl4028 <aviel4028@gmail.com>
2025-07-13 15:03:43 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Section } from "@radix-ui/themes";
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <Section style={{ border: "1px solid red" }}>
+                    <h1>Hi! I'm Aviel Flores</h1>
+            </Section>
+        </>
+    )
+}
+
+export default App
