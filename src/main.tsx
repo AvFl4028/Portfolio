dcca50d8dc31e1c1dc5f35896d0cd0664dc15f40
AvFl4028 <aviel4028@gmail.com>
2025-07-24 21:21:41 -0600

    production update

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Theme accentColor="purple" appearance="dark">
             <App />
-            <ThemePanel />
+            {/* <ThemePanel /> */}
         </Theme>
     </StrictMode>
 );
